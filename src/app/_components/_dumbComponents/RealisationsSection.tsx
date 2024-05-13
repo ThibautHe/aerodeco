ebd679de67a966338d7c5addc9ea0afb3c8cd87a
Tea-bow <thibaut.hellinckx@hotmail.com>
2024-05-13 18:23:29 +0200

    build changes

--- src/app/_components/_dumbComponents/RealisationsSection.tsx
+++ src/app/_components/_dumbComponents/RealisationsSection.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import ImageGrid from "./ImageGrid";
+import Image from "next/image";
 
 export default function RealisationsSection() {
   const images = [
@@ -12,10 +13,10 @@ export default function RealisationsSection() {
     <div className="mb-8">
       <h1 className="text-5xl font-bold text-center mb-16">Realisations</h1>
       <div className="flex flex-col sm:flex-row p-4 gap-16 justify-center items-center">
-        <img className="w-2/3 sm:w-1/3 p-4" src="/exemple/1.jpg" alt="" />
-        <img className="w-2/3 sm:w-1/3 p-4" src="/exemple/2.jpg" alt="" />
+        <Image className="w-2/3 sm:w-1/3 p-4" src="/exemple/1.jpg" alt="" />
+        <Image className="w-2/3 sm:w-1/3 p-4" src="/exemple/2.jpg" alt="" />
       </div>
-      <img className="m-auto w-3/4 my-32" src="/asset2.png" alt="" />
+      <Image className="m-auto w-3/4 my-32" src="/asset2.png" alt="" />
 
       <ImageGrid title={"Avant / Apres"} images={images}></ImageGrid>
 
