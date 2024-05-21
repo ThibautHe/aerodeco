bb3e4ee0dd1d5e71e5dd67d6ee05457eb8ca1868
Tea-bow <thibaut.hellinckx@hotmail.com>
2024-05-21 18:01:57 +0200

    added key

--- src/app/_components/_dumbComponents/ImageGrid.tsx
+++ src/app/_components/_dumbComponents/ImageGrid.tsx
@@ -17,9 +17,13 @@ export default function ImageGrid({ prefix, title, images }: imageGridProps) {
     <div className="grid grid-cols-phone sm:grid-cols-layout1 p-4 sm:gap-16 justify-center w-full">
       <h1 className="text-2xl p-4 col-span-2">{title}</h1>
       {images.map((image) => (
-        <motion.div initial={{ y: 110,opacity:0 }} whileInView={{ y: 0,opacity:1 }} transition={{duration:1}}>
+        <motion.div
+          key={image}
+          initial={{ y: 110, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
           <Image
-            key={image}
             width={400}
             height={400}
             className="w-full p-4"
